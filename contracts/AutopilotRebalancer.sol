e5f1c553263f2ae2bb1f061d76ffbfd6a7e811f5
vpacifist <v@vovagusev.ru>
2026-06-04 07:01:47 +0200

    Restrict rebalancer to WETH USDC positions

--- contracts/AutopilotRebalancer.sol
+++ contracts/AutopilotRebalancer.sol
@@ -41,6 +41,23 @@ interface INonfungiblePositionManager {
     function collect(CollectParams calldata params) external payable returns (uint256 amount0, uint256 amount1);
     function mint(MintParams calldata params) external payable returns (uint256 tokenId, uint128 liquidity, uint256 amount0, uint256 amount1);
     function ownerOf(uint256 tokenId) external view returns (address owner);
+    function positions(uint256 tokenId)
+        external
+        view
+        returns (
+            uint96 nonce,
+            address operator,
+            address token0,
+            address token1,
+            uint24 fee,
+            int24 tickLower,
+            int24 tickUpper,
+            uint128 liquidity,
+            uint256 feeGrowthInside0LastX128,
+            uint256 feeGrowthInside1LastX128,
+            uint128 tokensOwed0,
+            uint128 tokensOwed1
+        );
 }
 
 interface ISwapRouter02 {
@@ -68,6 +85,7 @@ contract AutopilotRebalancer {
     error InvalidAmount();
     error InvalidTokenOrder();
     error NotVaultPosition();
+    error UnsupportedPosition();
     error UnsupportedSwapTarget();
     error SwapFailed(bytes data);
     error InsufficientSwapOutput(uint256 amountOut, uint256 amountOutMinimum);
@@ -189,6 +207,7 @@ contract AutopilotRebalancer {
 
         if (params.closePosition.liquidity == 0) revert InvalidAmount();
         if (positionManager.ownerOf(params.closePosition.tokenId) != vault) revert NotVaultPosition();
+        _validateClosePosition(params.closePosition);
         positionManager.decreaseLiquidity(
             INonfungiblePositionManager.DecreaseLiquidityParams({
                 tokenId: params.closePosition.tokenId,
@@ -241,6 +260,26 @@ contract AutopilotRebalancer {
     function sweepToken(address token) external onlyOwner nonReentrant {
         _validateToken(token);
         _refund(token);
+    }
+
+    function _validateClosePosition(CloseParams calldata closePosition) private view {
+        (
+            ,
+            ,
+            address token0,
+            address token1,
+            uint24 fee,
+            int24 tickLower,
+            int24 tickUpper,
+            uint128 liquidity,
+            ,
+            ,
+            ,
+            ) = positionManager.positions(closePosition.tokenId);
+
+        if (token0 != weth || token1 != usdc || fee != POOL_FEE) revert UnsupportedPosition();
+        if (tickLower >= tickUpper) revert UnsupportedPosition();
+        if (liquidity < closePosition.liquidity) revert InvalidAmount();
     }
 
     function _validateMint(MintParams calldata mintPosition) private view {
